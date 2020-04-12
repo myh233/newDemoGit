b38817a7a4c07a525bad496b178d663742cc84e4
meiyuhang <1263598058@qq.com>
2020-04-12 16:21:56 +0800

    第n次提交

--- widgit/script/reg.js
+++ widgit/script/reg.js
@@ -1,5 +1,5 @@
 //注册函数
-/*function reg(){
+function reg(){
 	var model = api.require("model");
 	model.config({
 		appKey: '74C42CC5-1778-E823-BCD7-B12A156D2696',
@@ -23,34 +23,4 @@
        //alert( JSON.stringify( err ) );
     }
   });
-}*/
-
-//注册函数二
-function reg(){
-	var username=document.getElementById('username').value;
-  var password=document.getElementById('password').value;
-	/*var username = api.byId('username');
-	var password = api.byId('password');
-	var vusername = api.val(username);
-	var vpassword = api.val(password);*/
-	api.ajax({
-		url:'https://d.apicloud.com/mcm/api/user',
-		method:'post',
-		headers:{
-			"X-APICloud-AppId":"A6038101288292",
-			"X-APICloud-AppKey":"74C42CC5-1778-E823-BCD7-B12A156D2696"
-		},
-		data:{
-			values:{
-				username:username,
-				password:password
-			}},
-			function(ret,err){
-				if(ret && ret.id){
-					alert("注册成功!");
-				}else{
-					alert("注册失败!");
-				}
-			}
-	});
 }
